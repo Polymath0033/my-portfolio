eaac03bee19d2ba2dac9449d8e8571c4e86fbe26
Yousouph Olosan <depolymathyousouph@gmail.com>
2025-03-19 09:17:31 +0100

    just a quick commit

---
+++ src/components/layouts/side-layout.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+console.log('Hello from SideLayout');
+</script>
+<template>
+    <aside class="md:w-[200px] min-[1100px]:w-[250px] border-r border-border h-full " >
+        <slot/>
+    </aside>
+</template>
